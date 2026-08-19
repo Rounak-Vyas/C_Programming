058b3cd1562a8c550793739e1aa3f4e3d5dca1ad
Rounak Vyas <1nt23ee045.rounak@nmit.ac.in>
2026-08-20 00:14:31 +0530

    Implemented basic program to show the flow of header files exceution process

---
+++ Header Files/p1/changefile.c
@@ -0,0 +1,5 @@
+
+#include <stdio.h> // only to include if we are using any standard library functions in our custom functions
+void changefile(){
+    printf("The compiler now executes the content of changed file\n");
+}
---
+++ Header Files/p1/main.c
@@ -0,0 +1,10 @@
+// This is the main file 
+
+#include <stdio.h>
+#include "changefile.h"
+
+int main (){
+    printf("Currently under main file\n ");
+    changefile();
+    return 0;
+}
